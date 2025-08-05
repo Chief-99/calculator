158ca620df2fe3dd3688ecdcf4bda1868924063a
Similo <171865838+Chief-99@users.noreply.github.com>
2025-08-05 17:13:11 +0200

    Write function to give operation result based on operator

--- calculator.js
+++ calculator.js
@@ -2,6 +2,26 @@ let firstNumber;
 let operator;
 let secondNumber;
 
+function operate(operator, first, second) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(first, second);
+            break;
+        case '-':
+            result = subtract(first, second);
+            break;
+        case '*':
+            result = multiply(first, second);
+            break;
+        case '/':
+            result = divide(first, second);
+            break;
+    }
+
+    return result;
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -17,3 +37,8 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+firstNumber = 12;
+secondNumber = 4;
+operator = '+';
+console.log(operate(operator, firstNumber, secondNumber));
